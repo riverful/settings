160d2f3dd72f602b5038ad02b1feff952e487d7b
HeungJun (Jeremy), Kim <heungjun@qti.qualcomm.com>
2018-07-26 02:42:15 +0900

    Add scripts

---
+++ script_core.sh
@@ -0,0 +1,310 @@
+#!/bin/bash
+
+#########################################################
+# core func
+#########################################################
+
+set_java_path() {
+  if [ "$1" = "17" ]; then
+    export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+  elif [ "$1" = "18" ]; then
+    export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64"
+  else
+    export JAVA_HOME="/usr/lib/jvm/java-7-openjdk-amd64"
+  fi
+
+  export ANDROID_JAVA_HOME=$JAVA_HOME
+  export CLASSPATH="${JAVA_HOME}/lib"
+  export PATH="${PATH}:${ANDROID_JAVA_HOME}/bin:$HOME/Library/Android/sdk/platform-tools"
+  echo "[script] Set java path: [$JAVA_HOME]"
+}
+set_alias() {
+  # usage of rsync
+  #   rsync -azrt --exclude '~/xxx' --exclude 'yyy' /mnt/to/ /mnt/from/
+  # -e 'ssh -p 2222'
+
+  OPTS_RSYNC="-avzogc --stats"
+  OPTS_RSYNC_DELETE="-avzogc --stats --delete"
+  OPTS_RSYNC_SSH="${OPTS_RSYNC} -e 'ssh -p 2222'"
+  OPTS_RSYNC_SSH_DELETE="${OPTS_RSYNC_DELETE} -e 'ssh -p 2222'"
+
+  alias rsyncs="sudo rsync ${OPTS_RSYNC_SSH}"
+  alias rsyncs_sync="sudo rsync ${OPTS_RSYNC_SSH_DELETE}"
+
+  alias du_sh_sort="du -sh * | sort -nr"
+  alias du_sk_sort="du -sk * | sort -nr"
+  alias adb_host='${HOST_ADB}'
+  alias fastboot_host='${HOST_FASTBOOT}'
+
+  alias screen_new="screen -s bash -S "
+  alias screen_ls="screen -ls"
+  alias screen_attach="screen -dr "
+
+  alias tmxnew="tmux -2 new -s "
+  alias tmxls="tmux -2 ls"
+  alias tmxattach="tmux -2 attach -t "
+
+  alias ls='ls -hF --color=tty'
+  alias grepc="GREP_OPTIONS='color=auto grep"
+}
+
+#########################################################
+# install packages
+#########################################################
+
+uninstall_unused_linux_image() {
+  dpkg --get-selections | grep 'linux-image*' | awk '{print $1}' | \
+    egrep -v "linux-image-$(uname -r)|linux-image-generic" | \
+    while read n; do $CMD_APT -y remove $n; done
+}
+
+install_java() {
+  if [ "$1" = "" ]; then
+    echo "Usage: install_java [17/18]"
+    return
+  fi
+
+  if [[ "$1" == "17" ]]; then
+    local _ver_jdk="7"
+  elif [[ "$1" == "18" ]]; then
+    local _ver_jdk="8"
+  else
+    local _ver_jdk="8"
+  fi
+
+  PKG_JDK="openjdk-${_ver_jdk}-jdk"
+  $CMD_APT_ADD_REPO ppa:openjdk-r/ppa -y
+  $CMD_APT update
+  $CMD_APT install $PKG_JDK -y
+}
+install_dev_android() {
+  install_java 17
+  install_java 18
+  $CMD_APT install git-core gnupg flex bison gperf build-essential \
+    zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 \
+    lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache \
+    libgl1-mesa-dev libxml2-utils xsltproc unzip -y
+  $CMD_APT autoremove
+}
+install_default() {
+  if [[ "$UBUNTU_VERSION" == *"14.04"* ]]; then
+    $CMD_APT_ADD_REPO ppa:jonathonf/vim -y # newest vim
+  fi
+
+  $CMD_APT update
+
+  $CMD_APT install cmake clang vim ctags cscope htop tree screen python-dev \
+    gparted python-pip synapse jq -y
+
+  pip install --upgrad pip
+
+  IS_UBUNTU_DESKTOP=`dpkg -l ubuntu-desktop`
+  if [[ "$IS_UBUNTU_DESKTOP" == *"dpkg-query: no packages found"* ]]; then
+    echo "[scripts] Ubuntu Server setting"
+    $CMD_APT install openssh-server rdesktop sysstat -y # Server
+
+  elif [[ "$IS_UBUNTU_DESKTOP" =~ "The Ubuntu desktop system" ]]; then
+    echo "[scripts] Ubuntu Desktop setting"
+    $CMD_APT install unity-tweak-tool gnome-tweak-tool synapse numix-* \
+      compizconfig-settings-manager gconf-editor -y # Desktop
+  fi
+
+  $CMD_APT autoremove
+}
+install_all_pkgs() {
+  install_default
+  install_env
+  install_dev_android
+}
+
+
+#########################################################
+# util
+#########################################################
+
+parse_git_branch () {
+  git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
+}
+get_pid() {
+  local NAME_DAEMON=$1
+  local PID_DAEMON=`ps -ef | grep $NAME_DAEMON | grep -v 'grep' | sed 's/ \{1,10\}/:/g' | sed 's/^://g' | cut -f 2 -d ":"`
+  for pid in $PID_DAEMON; do
+    echo "[script] get_pid: $pid"
+  done
+}
+set_ssh_agent() {
+  local NAME_DAEMON='ssh-agent'
+  local PID_DAEMON=`ps -ef | grep $NAME_DAEMON | grep -v 'grep' | sed 's/ \{1,10\}/:/g' | sed 's/^://g' | cut -f 2 -d ":"`
+
+  # check all pid from ssh-agent
+  for pid in $PID_DAEMON; do
+    echo "[script] killed : $pid"
+    kill -9 $pid
+  done
+
+  # ssh-add newly
+  eval "$(ssh-agent -s)"
+  for key_id in `ls $HOME/.ssh/id_rsa* | grep -v '.pub'`; do
+    ssh-add $key_id
+  done
+  echo "[script] Set ssh-agent"
+}
+timetake() {
+  time sh -c "$1"
+}
+io_check() {
+  local BANDWIDTH=`dd if=/dev/zero of=./tmpfile count=4000 bs=1024k | grep copied`
+  rm -rf ./tmpfile
+  echo "[script] bandwidth $BANDWIDTH"
+}
+port_check() {
+  netstat -tnlp
+  nmap localhost
+}
+git_cleanup_oversize_lfs() {
+  if [ -f $HOME/binary ] ; then
+    java -jar $HOME/binary/archive/bfg-1.12.14.jar --strip-blobs-bigger-than 30M
+  fi
+
+  git reflog expire --expire=now --all
+  git gc --prune=now --aggressive
+}
+check_last_fail() {
+  local LAST_FAIL_CHECK="sudo last -f /var/log/btmp"
+  local FAILED_IP=`$LAST_FAIL_CHECK | cut -f 10 -d " " | head -n 1`
+  echo "[script] problematic ip : $FAILED_IP"
+  $LAST_FAIL_CHECK
+  echo "[script] whois ip : $FAILED_IP........"
+  whois $FAILED_IP
+}
+compiz_reset() {
+  gconftool-2 --recursive-unset /apps/compiz-1
+  gconftool-2 --recursive-unset /apps/compizconfig-1
+  unity --reset
+}
+java_change() {
+  if [ "$1" = "" ]; then
+    echo "Usage: java_change [17/18]"
+    return
+  fi
+
+  execute_script $1
+
+  sudo update-alternatives --config java
+  sudo update-alternatives --config javac
+
+  java -version
+}
+git_recover() {
+  git fsck; git gc; git prune; git repack; git fsck;
+}
+git_solve_conflict() {
+ vi `git diff &> log && cat log | grep "diff" | cut -f 3 -d " "` && rm -rf diff
+}
+git_dev_master_commdiffs() {
+ git l dev &> tmp && sed '/Merge/d' tmp > dev_ && git l master_au &> tmp && sed '/Merge/d' tmp > au_ && vimdiff dev_ au_
+}
+git_dev_master_diffs() {
+#  MASTER=`git log --pretty=format:'%H' $1 | sort`
+#  DEV=`git log --pretty=format:'%H' $2 | sort`
+  local MASTER=`git log --pretty=format:'%H' $1`
+  local DEV=`git log --pretty=format:'%H' $2`
+  for i in `diff <(echo "${MASTER}") <(echo "${DEV}") | grep '^>' | sed 's/^> //'`;
+  do
+    git --no-pager log -1 --oneline $i | sed '/Merge/d' ;
+  done
+}
+mmcli_status() {
+  mmcli -m 0
+}
+mmcli_register_home() {
+  mmcli -m 0 --3gpp-register-home
+}
+
+
+#########################################################
+# temp
+#########################################################
+
+is_file() {
+  [ -f "$1" ]
+}
+is_file0() {
+  return 0
+}
+is_file1() {
+  return 1
+}
+is_test_folder() {
+  if test -e $1
+  then
+    echo "Yes"
+  else
+    echo "No"
+  fi
+}
+test_temp() {
+  if is_file0 || is_file1 ; then
+    echo "Exist"
+  else
+    echo "No"
+  fi
+}
+test_check_num_char() {
+  if [ $# -ne 1 ] ; then
+    echo "Usage: $ test_check_num_char [input]"
+    echo "    $ check_num_char aaaa"
+    echo "    $ check_num_char 123411"
+    return;
+  else
+    local _var=$1
+  fi
+
+  if  [ $_var -eq $_var 2> /dev/null ]; then
+    echo "number"
+  else
+    echo "no number"
+  fi
+}
+
+
+#########################################################
+# manage scripts
+#########################################################
+
+pgit() {
+  cd $HOME/$2
+  git push $1 master
+}
+ugit() {
+  if [ "$1" = "" -o "$2" = "" ]; then
+    echo "Usage: ugit [origin] [branch]"
+    return
+  fi
+  local NOW=$(date +"%Y-%m-%d %H:%M:%S")
+  cd $HOME/$2
+  git remote update $1
+  git fetch $1
+  git co master
+  git add .
+  git commit -a -m "$NOW"
+  git pull $1 master
+  pgit $1 $2
+}
+update_scripts_core() {
+  cd
+  wget https://github.com/riverful/settings/raw/master/scripts_default.sh
+  wget https://github.com/riverful/settings/raw/master/scripts_core.sh
+  mv scripts_default.sh ~/scripts/
+  mv scripts_core.sh ~/scripts/
+}
+
+
+#########################################################
+# excute
+#########################################################
+
+source $HOME/scripts/script_default.sh $1
+
+echo "[script] Load core script"
+
---
+++ script_default.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+
+#########################################################
+# core func
+#########################################################
+
+set_shell_env() {
+  if [[ "$UBUNTU_VERSION" == *"14.04"* ]]; then
+    CMD_APT="sudo apt-get"
+  else
+    CMD_APT="sudo apt"
+  fi
+  CMD_APT_ADD_REPO="sudo add-apt-repository"
+
+  stty erase ^H
+  stty erase ^?
+
+  export PS1="[\[\e[35;40m\]\t\[\e[0m\]] \[\033[0;94m\]\u@\h \[\e[1;32m\]\w \[\e[0m\] \n \$ "
+
+  echo "[script] Set default env - PS1"
+}
+set_gitconfig() {
+  git config --global user.name "none"
+  git config --global user.email "noname@noname.com"
+  git config --global core.fileMode false
+  git config --global branch.current "yellow reverse"
+  git config --global branch.local "yellow"
+  git config --global branch.remote "green"
+  git config --global diff.meta "yellow bold"
+  git config --global diff.frag "magenta bold"
+  git config --global diff.old "red bold"
+  git config --global diff.new "green bold"
+  git config --global status.added "yellow"
+  git config --global status.changed "green"
+  git config --global status.untracked "cyan"
+  git config --global color.ui true
+  git config --global core.whitespace "fix,-indent-with-non-tab,trailing-space,cr-at-eol"
+  git config --global core.editor vim
+  git config --global alias.co checkout
+  git config --global alias.br branch
+  git config --global alias.lg "log -p"
+  git config --global alias.lla "log --graph --decorate --pretty=oneline --abbrev-commit --all"
+  git config --global alias.ls "log --stat"
+  git config --global alias.ll "log --pretty=format:\"%C\(yellow\)%h %C(cyan)%\>\(12\)%cd %Cgreen%\<\(20,trunc\)%aN %Creset%s %C\(red\)%d\" --date=short --graph"
+  git config --global alias.ld "log -p --stat"
+  git config --global alias.l "log --pretty=format:\"%C\(yellow\)%h %C(cyan)%\>\(12\)%cd %Cgreen%\<\(20,trunc\)%aN %Creset%s %C\(red\)%d\" --date=short"
+
+  git config --global --replace-all core.fileMode false
+  git config --global core.whitespace trailing-space
+#  git config --global core.autocrlf false
+
+  git config --global merge.tool vimdiff
+  git config --global merge.conflictstyle diff3
+  git config --global mergetool.prompt false
+
+}
+set_default_env() {
+  export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_75"
+  export ANDROID_JAVA_HOME=$JAVA_HOME
+  export CLASSPATH="${JAVA_HOME}/lib"
+  export PATH="${ANDROID_JAVA_HOME}/bin:~/binary/archive:~/scripts:~/binary/tools:~/binary/archive:${PATH}"
+
+  if [ "$1" = "y" ]; then
+    set_gitconfig
+  fi
+
+  alias mkdird="mkdir `date '+%m%d'` ; cd `date '+%m%d'`"
+
+  export CONF_ENV='None'
+}
+
+#########################################################
+# Android
+
+reposetdir() {
+  export _ANDROID_ROOT="`pwd`"
+
+  echo "[script] _ANDROID_ROOT [$_ANDROID_ROOT]"
+
+  local MACH=`uname`
+  if [ $MACH = 'Linux' ]; then
+    local _RPATH_CCACHE=prebuilts/misc/linux-x86/ccache/
+  elif [ $MACH = 'Darwin' ]; then
+    local _RPATH_CCACHE=prebuilts/misc/darwin-x86/ccache/
+  fi
+  local _PATH_CCACHE="${_ANDROID_ROOT}/${_RPATH_CCACHE}"
+
+  if [ -d $_PATH_CCACHE ]; then
+    export USE_CCACHE=1
+    export CCACHE_DIR="$_ANDROID_ROOT/.ccache"
+    $_PATH_CCACHE/ccache -M 50G
+    ulimit -S -n 1024
+    echo "[script] ccache [$_PATH_CCACHE/ccache]"
+  fi
+
+  if [ $MACH = 'Linux' ]; then
+    local HOST_TOOL="${_ANDROID_ROOT}/out/host/linux-x86/bin/"
+    export HOST_ADB="sudo ${HOST_TOOL}adb"
+    export HOST_FASTBOOT="sudo ${HOST_TOOL}fastboot"
+  else
+    local HOST_TOOL=""
+    export HOST_ADB="${HOST_TOOL}adb"
+    export HOST_FASTBOOT="${HOST_TOOL}fastboot"
+  fi
+
+  cd "${_ANDROID_ROOT}"
+}
+repolunch() {
+  if [ $CONF_ENV != 'None' ]; then
+    return
+  fi
+
+  reposetdir
+  cd $_ANDROID_ROOT
+
+  echo "[script] lunching......"
+
+  source build/envsetup.sh
+
+  export `grep -r "TARGET_PRODUCT" ../buildscript/build.log | head -n 1`
+  lunch `grep -r "lunch" ../buildscript/build.log | tail -n 1 | cut -f 4 -d " "`
+}
+
+adb_wait_clean() {
+  $HOST_ADB wait-for-device
+  $HOST_ADB root
+  $HOST_ADB logcat -c
+}
+adb_log_grep() {
+  if [ "$1" = "" ]; then
+    echo "Usage: adb_log_grep [string]"
+    return
+  fi
+  adb_wait_clean
+  $HOST_ADB logcat -vthreadtime | tee tmplog | grep "$1"
+}
+adb_setprop_rawdump() {
+  adb_wait_clean
+  $HOST_ADB root
+  $HOST_ADB shell su -c setenforce 0
+  $HOST_ADB shell setprop persist.camera.feature.osd 1
+  $HOST_ADB shell setprop persist.camera.cal.awb 0
+  $HOST_ADB shell setprop persist.camera.cal.lsc 0
+  $HOST_ADB shell su -c setenforce 0
+  $HOST_ADB shell chmod 777 /data
+  $HOST_ADB shell chmod 777 /data/misc/camera
+  $HOST_ADB shell setprop persist.camera.dumpimg 1310740
+  $HOST_ADB shell setprop persist.camera.raw_yuv 1
+  $HOST_ADB shell setprop persist.camera.zsl_raw 1
+  $HOST_ADB shell setprop persist.camera.zsl_yuv 1
+  $HOST_ADB shell setprop persist.camera.dumpmetadata 1
+}
+adb_key_back() {
+  adb_wait_clean
+  $HOST_ADB shell input keyevent 4
+}
+
+
+#########################################################
+# setup environment
+#########################################################
+install_env() {
+  rm -rf _env_temp
+  mkdir _env_temp
+  cd _env_temp
+
+  wget https://github.com/riverful/settings/raw/master/screenrc
+  wget https://github.com/riverful/settings/raw/master/vimrc
+  wget https://github.com/riverful/settings/raw/master/cscope_maps.vim
+  wget https://github.com/riverful/settings/raw/master/taglist.vim
+
+  chmod 644 screenrc
+  chmod 644 vimrc
+  chmod 644 cscope_maps.vim
+  chmod 644 taglist.vim
+
+  rm -rf ~/.vim/plugin
+  rm -rf ~/.vim
+  rm -rf ~/.vimrc
+  rm -rf ~/.screenrc
+  rm -rf ~/.tmux.conf
+
+  mkdir ~/.vim
+  mkdir ~/.vim/plugin
+
+  mv screenrc ~/.screenrc
+  mv vimrc ~/.vimrc
+  mv cscope_maps.vim ~/.vim/plugin/cscope_maps.vim
+  mv taglist.vim ~/.vim/plugin/taglist.vim
+
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+  vim +PluginInstall +qall
+
+  cd ..
+  rm -rf _env_temp
+}
+
+
+#########################################################
+# util
+#########################################################
+mk_cscope_ctags() {
+  path_root_cscope=""
+  if [ "$1" = "." ] ; then
+    local path_root_cscope="."
+    echo "[script] mk_cscope_ctags ALL: ${path_root_cscope}"
+  elif [ -f $1 ] ; then
+    while read _file_path
+    do
+      path_root_cscope+="$_file_path "
+    done < $1
+    echo "[script] mk_cscope_ctags from list file: $1, path: ${path_root_cscope}"
+  else
+    echo "[script] mk_cscope_ctags: no input"
+  fi
+
+  rm -rf cscope.files cscope.out tags
+  find $path_root_cscope -type f \( -name '*.py' -o -name '*.c' -o -name '*.cpp' -o -name '*.cc' -o -name '*.h' -o -name '*.s' -o -name '*.S' \) ! \( -path "*.git*" \) -print > cscope.files
+  cscope -b -i cscope.files
+  ctags -L cscope.files
+  ctags cscope.files
+}
+dosunix() {
+  dos2unix -f $1
+  perl -pi -e 's/\r$//g' $1
+  chmod 644 $1
+}
+dosunix_files() {
+  find . -name "*.c" -not -path ".git" -exec bash -c 'dosunix "$0"' {} \;
+  find . -name "*.cpp" -not -path ".git" -exec bash -c 'dosunix "$0"' {} \;
+  find . -name "*.h" -not -path ".git" -exec bash -c 'dosunix "$0"' {} \;
+  find . -name "Android.mk" -not -path ".git" -exec bash -c 'dosunix "$0"' {} \;
+  find . -name "Makefile" -not -path ".git" -exec bash -c 'dosunix "$0"' {} \;
+}
+gitcreate() {
+  wget https://github.com/riverful/settings/raw/master/gitignore
+  mv gitignore .gitignore
+
+  dosunix_files
+
+  git init
+  git add .
+  git config core.fileMode false
+  git commit -a -m "Init"
+}
+
+
+#########################################################
+# excute
+#########################################################
+
+git_replace_apply="n"
+
+if [ $# -eq 1 ]; then
+  if [ "$1" = "y" ]; then
+    git_replace_apply="y"
+  fi
+fi
+
+set_default_env $git_replace_apply
+set_shell_env
+
+echo "[script] Load default script"
+
