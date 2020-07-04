1abf9c29a64fcfd42e3ba5da844720803ee81734
Jeremy <riverful@gmail.com>
2020-07-05 00:11:00 +0900

    Update _default.sh

--- _default.sh
+++ _default.sh
@@ -27,7 +27,6 @@ _set_default_env() {
   export JAVA_HOME="/usr/lib/jvm/jdk1.7.0_75"
   export ANDROID_JAVA_HOME=$JAVA_HOME
   export CLASSPATH="${JAVA_HOME}/lib"
-  export _SCRIPTS=".scripts"
   export PATH="${ANDROID_JAVA_HOME}/bin:~/$_SCRIPTS:${PATH}"
 
   alias mkdird="mkdir `date '+%m%d'` ; cd `date '+%m%d'`"
